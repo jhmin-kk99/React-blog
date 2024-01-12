0925d207cda12fcdd66b3a25a9a1962d784c4c31
jihong <jihongmin99@naver.com>
2024-01-12 15:19:10 +0900

    JSX 기초문법

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+//JSX 언어 (.js파일에서 쓰는 html대용품)
+// class 대신 className
+// 변수넣을 땐 {중괄호} (databinding)
+// style 넣을 땐 style = {{스타일명: '값'}}
 function App() {
+
+  let post = '강남 우동 맛집';
+  //서버에서 가져온 데이터라 가정
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={ {color : 'red', fontSize : '16px'} }>블로그임</h4>
+      </div>
+      <h4>{ post }</h4>
     </div>
   );
 }
