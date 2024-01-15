c9822155f4683dcd6dadc542fd025c131ee42326
jihong <jihongmin99@naver.com>
2024-01-16 00:16:42 +0900

    array, object state 변경

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   let post = '강남 우동 맛집';
   //서버에서 가져온 데이터라 가정
 
-  let [글제목, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
   // 1. import {useState}
   // 2. useState(보관할 자료)
   // 3. let[작명, 작명]
@@ -35,13 +35,35 @@ function App() {
  
 
   //return()안에는 병렬로 태그 2개이상 기입 금지
+  //array/object 다룰 때 원본은 보존하는게 좋음
+  //state변경함수 특징 -> 기존state == 신규state의 경우 변경안해줌 (자원절약)
+  //array/object 담은 변수엔 화살표만 저장됨 ([1,2,3]은 램에 저장되어있음)
+  //변수1 & 변수2 화살표가 같으면, 변수1==변수2 비교해도 true나옴
+  //array, object는 reference data type임
+  
+  //let copy = [...글제목]  (괄호 벗겨주세요 -> 화살표 새로 바꿔주세요 -> 괄호를 다시 씌워주세요)
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
 
-      <button onClick={ () => { {b(['여자코트 추천', '강남 우동맛집', '파이썬 독학'])} }}></button>
+      <button onClick={
+        () => {
+          let copy = [...글제목];
+          copy.sort();
+          글제목변경(copy);
+
+        }
+      }>가나다순정렬</button>
+
+      <button onClick={ () => {
+        
+        let copy = [...글제목];
+        copy[0] = '여자코트 추천';
+        글제목변경(copy);        
+        
+        }}>글수정</button>
 
       <div className='list'>
         <h4>{ 글제목[0] } <span onClick={ () => {{ 따봉변경(따봉+1) }} }>👍</span> { 따봉 } </h4>
